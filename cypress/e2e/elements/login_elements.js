0e9955091a6f37ae4dd212ba998f0e308be12c11
Guilherme Campos <157999298+guilhermecampos-dev@users.noreply.github.com>
2026-02-15 13:22:57 -0300

    feat: add login elements structure

---
+++ cypress/e2e/elements/login_elements.js
@@ -0,0 +1,8 @@
+class LoginElements {
+    
+    inputUserName = () => { return 'input[placeholder="Username"]' };
+    inputPassword = () => { return 'input[placeholder="Password"]'};
+    btnLogin = () => { return 'button[type="submit"]'};
+
+
+} export default LoginElements
